19966b74187bb260beb9ed37bae5d230dc86c6a0
Brandon Harbican <imjust_don@byui.edu>
2023-03-30 21:05:56 -0600

    kljhlkjhlkjljkh

--- week 13/milestone.py
+++ week 13/milestone.py
@@ -1,10 +1,11 @@
 def wind_chill(temperature):
     count = 0
-    wind_speed = 0
-    while count <= 12:
+    wind_speed = 0.0
+    while count <= 11:
         wind_speed += 5
-        real_feel = 35.74 + (0.6215*temperature) - (35.75(wind_speed**0.16)) + ((0.4275*temperature)(wind_speed**0.16))
-        print(f'At the temperature: {temperature}F, and the windspeed of: {wind_speed} MPH, the windchill is: {real_feel}F')
+        real_feel = 35.74 + (0.6215*temperature) - (35.75*(wind_speed**0.16)) + ((0.4275*temperature)*(wind_speed**0.16))
+        print(f'At the temperature: {temperature}F, and the windspeed of: {wind_speed} MPH, the windchill is: {real_feel:.2f} Farenheit')
+        count += 1
 
 
 temp = float(input('What is the temperatur outside? '))
