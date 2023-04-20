4267afc8bac4d5a5092cba9dd10a1badf3a1a3e1
Brandon Harbican <blharbican@outlook.com>
2023-04-20 15:41:27 -0600

    hgjhgjhg

--- C/week 1/vairiables and stuff.c
+++ C/week 1/vairiables and stuff.c
@@ -2,5 +2,30 @@
 #include <stdlib.h>
 
 int main(void) {
-    int i = 0;
+    int i = 17;
+    short s = 15;
+    long l = 60;
+    char c = 'a';
+
+
+    printf("The number is %d. The size is %ld\n", i, sizeof(i));
+    printf("The character is %c, the numeric value is %d, The size is %ld\n", c, c, sizeof(c));
+
+    return EXIT_SUCCESS;
+}
+
+int nun(void) {
+    int a[5];
+
+    a[0] = 6;
+    a[2] = 34;
+    a[3] = 1234;
+    a[4] = 4563;
+
+    printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]);
+
+    char name[15] = "Harry";
+    printf("The name is %s\n", name);
+
+    return EXIT_SUCCESS;
 }
