dd534eeaa77eef19847c636e3ac4ea66f2088551
Brandon Harbican <imjust_don@byui.edu>
2023-04-22 12:12:21 -0600

    xcvb

--- C/week 1/assingment.c
+++ C/week 1/assingment.c
@@ -5,13 +5,37 @@ int main(void) {
     int i = 0;
     char name[20];
 
+    printf("\n");
     printf("Please enter a number: ");
     scanf("%d", &i);
+    printf("\n");
     printf("Your number is %d\n", i);
+    printf("\n");
     printf("Please enter a name: ");
     scanf("%s", name);
     printf("The name you entered is %s\n", name);
+    printf("\n");
 
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 < num2) {
+        printf("%d is the minimum value.\n", num1);
+    }
+    else {
+        printf("%d is the minimum value.\n", num2);
+    }
+
+    float num;
+    printf("Enter a floating point value: ");
+    scanf("%f", &num);
+
+    printf("-----------------------------\n");
+    printf("Scientific notation: %e\n", num);
+    printf("Fixed point notation: %f\n", num);
+    printf("Shortest representation: %g\n", num);
+    printf("-----------------------------\n");
 
     return EXIT_SUCCESS;
 }
