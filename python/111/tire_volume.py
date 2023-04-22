5b98ce8c86d828fd89a19a02c5da6eeebfaff770
Brandon Harbican <blharbican@outlook.com>
2023-04-22 09:45:39 -0600

    asdf

--- python/111/tire_volume.py
+++ python/111/tire_volume.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import math
 
 print()
@@ -6,8 +6,14 @@ width = int(input("Enter the width of the tire in mm (ex 205): "))
 aspect = int(input("Enter the aspect ratio of the tire (ex 60): "))
 diamater = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-answer = (math.pi * width**2 * aspect * (width * aspect + 2540 * diamater))/10000000000
+volume = (math.pi * width**2 * aspect * (width * aspect + 2540 * diamater))/10000000000
 
 print()
-print(f'The approximate volume is {answer:.2f} liters')
+print(f'The approximate volume is {volume:.2f} liters')
 print()
+
+file_object = open('volumes.txt', 'a')
+
+file_object.write(f"\n{datetime.now()}, {width}, {aspect}, {diamater}, {volume:.2f}")
+
+file_object.close()
