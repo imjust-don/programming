ef04936e419035478c7d9c7d8fb6694543846018
Brandon Harbican <imjust_don@byui.edu>
2023-05-17 21:28:38 -0600

    ghhj

--- python/111/week 6/receipt.py
+++ python/111/week 6/receipt.py
@@ -4,9 +4,24 @@ def main():
     key_index = 0
     name_index = 1
     price_index = 2
+    filename = "products.csv"
+    file2 = 'request.csv'
+    items = []
 
-    products_dict = read_dictionary('products.csv', key_index)
+    products_dict = read_dictionary(filename, key_index)
     print(products_dict)
+    print()
+    print()
+    print('Requested Items')
+    with open(file2, 'rt') as request:
+        reader = csv.reader(request)
+        next(reader)
+        for info in reader:
+            item = info[0]
+            thing = products_dict[item]
+            price = thing[price_index]
+            print(f'{thing[name_index]}: {info[1]} @ {price}')
+            items.append(info)
 
 
 
